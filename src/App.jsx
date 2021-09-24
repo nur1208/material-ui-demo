5fcd38a1a11b77c881498e26693b6fb037722225
nur <mode0o1208@yahoo.com>
2021-09-24 16:49:29 +0800

    DONE building responsive website UI using material-UI a.k.a MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,143 @@
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  ButtonGroup,
+  Button,
+  CardContent,
+  Link,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+import { useStyles } from "./style";
+
 function App() {
+  function Copyright() {
+    return (
+      <Typography
+        variant="body2"
+        color="textSecondary"
+        align="center"
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="https://mui.com/">
+          PhotoAlbum.com
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
+
+  const classes = useStyles();
+
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return (
-    <div className="App">
-      <h1>Working!</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm" className={classes.container}>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Something short and leading about the collection
+              below—its contents, the creator, etc. Make it short
+              and sweet, but not too short so folks don&apos;t
+              simply skip over it entirely.
+            </Typography>
+
+            <div className={classes.button}>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See My Photo
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xm={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                    >
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this
+                      section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      yes
+                    </Button>
+                    <Button size="small" color="primary">
+                      no
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+    </>
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,30 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  icon: {
+    margin: "20px",
+  },
+  button: {
+    marginTop: "40px",
+  },
+  cardGird: {
+    padding: "20px 0",
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: { paddingTop: "56.25%" },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
